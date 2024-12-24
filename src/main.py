567b24d06105f4b9f1a854dac5280fa092f84c2e
JonJLopez <jonlop1121@gmail.com>
2024-12-24 12:49:07 -0500

    add function copy files from a source to destination directory

--- src/main.py
+++ src/main.py
@@ -2,34 +2,27 @@ from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from inlinesplit import split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_children
 from extract_links import extract_markdown_images
+import os
+import shutil
+
+def copy_source_to_destination(src, dest):
+    paths_in_src = os.listdir(src)
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+
+    os.mkdir(dest)
+    for path in paths_in_src:
+        src_path = os.path.join(src, path)
+        print(f"currently copying {src_path} to {dest}")
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest)
+        else:
+            new_dest = os.path.join(dest, path)
+            copy_source_to_destination(src_path, new_dest)
+        
+
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.web.com")
-    htmlnode = HTMLNode("h1", "text inside")
-    leafnode = LeafNode("p", "text")
-    parentnode = ParentNode(None, [LeafNode("p", "text", {"key": "site"})],)
-
-    node = TextNode("code block", TextType.TEXT)
-    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-
-    node_link_image = TextNode(
-        "This is text with a link [to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)",
-        TextType.TEXT,
-    )
-    link_split = split_nodes_link([node_link_image])
-    print(link_split)
-    print(split_nodes_image(link_split))
-    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
-    text = "This is `some code` in the text"
-    p1 = ParentNode("code", [LeafNode(None, "some code")])
-    p2 = ParentNode("pre", [p1])
-    code_node = ParentNode("pre", text_to_children(text))
-    print(code_node.to_html())
-    nested_parent = ParentNode("pre", [ParentNode("code", [LeafNode(None, "This is some code")])])
-
-    print(p2.to_html())
-
+    copy_source_to_destination("static", "public")
 
 main()
